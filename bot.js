9d1fba0c7cc75265904ea360d6d008f7957e2b7b
Neil <nwong1250@gmail.com>
2020-01-21 11:29:34 -0800

    added code for testing requests to localhost server (buggy)

--- bot.js
+++ bot.js
@@ -1,13 +1,17 @@
-var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
+var HTTPS = require("https");
+var cool = require("cool-ascii-faces");
 
 var botID = process.env.BOT_ID;
 
 function respond() {
+  console.log(this.req.chunks);
+  console.log("respond function called");
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+    botRegex = /^\/cool guy$/;
 
-  if(request.text && botRegex.test(request.text)) {
+  console.log(`request: ${request}`)
+
+  if (request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
@@ -24,34 +28,33 @@ function postMessage() {
   botResponse = cool();
 
   options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
+    hostname: "api.groupme.com",
+    path: "/v3/bots/post",
+    method: "POST"
   };
 
   body = {
-    "bot_id" : botID,
-    "text" : botResponse
+    bot_id: botID,
+    text: botResponse
   };
 
-  console.log('sending ' + botResponse + ' to ' + botID);
+  console.log("sending " + botResponse + " to " + botID);
 
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
+  botReq = HTTPS.request(options, function (res) {
+    if (res.statusCode == 202) {
+      //neat
+    } else {
+      console.log("rejecting bad status code " + res.statusCode);
+    }
   });
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
+  botReq.on("error", function (err) {
+    console.log("error posting message " + JSON.stringify(err));
   });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
+  botReq.on("timeout", function (err) {
+    console.log("timeout posting message " + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
 }
-
 
 exports.respond = respond;
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ director = require("director");
 cool = require("cool-ascii-faces");
 bot = require("./bot.js");
 
+const queryString = require("querystring");
+
+port = Number(process.env.PORT || 5000);
 router = new director.http.Router({
   "/": {
     post: bot.respond,
@@ -12,22 +15,61 @@ router = new director.http.Router({
   }
 });
 
-server = http.createServer(function(req, res) {
+server = http.createServer(function (req, res) {
   req.chunks = [];
-  req.on("data", function(chunk) {
+  req.on("data", function (chunk) {
     req.chunks.push(chunk.toString());
   });
+  req.on('error', (error) => {
+    console.log(error);
+  })
 
-  router.dispatch(req, res, function(err) {
+  router.dispatch(req, res, function (err) {
     res.writeHead(err.status, { "Content-Type": "text/plain" });
     res.end(err.message);
   });
 });
 
-port = Number(process.env.PORT || 5000);
 server.listen(port);
+console.log("app running on https://127.0.0.1:5000/");
+
+// Start Testing //
+
+const postData = queryString.stringify({
+  msg: "/cool guy"
+});
+
+const options = {
+  hostname: "127.0.0.1",
+  port: 5000,
+  path: "/",
+  method: "POST"
+};
+
+console.log("moving to request");
+
+const req = http.request(options, res => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  res.setEncoding("utf8");
+  res.on("data", chunk => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on("end", () => {
+    console.log("No more data in response.");
+  });
+});
+
+req.on("error", e => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+req.write(postData);
+req.end();
+
+// End testing //
 
 function ping() {
   this.res.writeHead(200);
-  this.res.end("Hey, I'm DSP Bot.");
+  this.res.end("Hey, I'm DSP Bot!");
 }
