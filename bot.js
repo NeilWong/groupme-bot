6ac434b37273902a2a054016c892f4111d984fa3
Neil <nwong1250@gmail.com>
2020-01-23 18:54:27 -0800

    added changes for code

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
+let http = require("http");
 let constants = require("./constants.js");
-let apis = require("./apis/messages")
+let apis = require("./apis/messages");
 
 // Process: receive message (respond) -> verify valid message (checkMessage) -> post correct message in response (postMessage)
 
@@ -8,13 +9,8 @@ let apis = require("./apis/messages")
  */
 function respond() {
   // let request = JSON.parse(this.req.chunks[0])
-  // let test = JSON.parse(this.req.chunks)
-  // console.log(request);
-  // console.log(test)
-  // console.log(test.name);
-  // console.log(test.user_id);
   let request = JSON.parse(this.req);
-  let message = request.text //+ request.name + request.user_id;
+  let message = request.name;
   let commands = constants.VALID_MESSAGES;
 
   // if (request['name'] === "DSP-Bot" && count < 2) {
@@ -29,7 +25,7 @@ function respond() {
     apis.postMessage(message, true);
     this.res.end();
   } else {
-    console.log('ignore message')
+    console.log("ignore message");
     this.res.writeHead(200);
     this.res.end();
   }
@@ -37,12 +33,12 @@ function respond() {
 
 /**
  * Checks if a request message is valid by checking if it is in a specified list of valid commands
- * @param {String} message 
- * @returns {bool} 
+ * @param {String} message
+ * @returns {bool}
  */
 const isValidMessage = (message, commands) => {
   return commands.includes(message);
-}
+};
 
 // Start Helper Functions //
 
